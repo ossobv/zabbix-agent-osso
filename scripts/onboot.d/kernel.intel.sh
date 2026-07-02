388ce98aaedb48bb5ced94c44dfc97471b0e24d6
Walter Doekes <walter+github@wjd.nu>
2026-07-02 11:18:55 +0200

    onboot/kernel.intel: Per PCI address, read the latest state
    It turns out we can certainly load new firmware, but only on systems
    that are not completely bogged down by memory requests. (In our case,
    looking at /proc/buddyinfo showed 0 and 0 in the last two columns of
    all CPU nodes Normal zone. By stopping the workload, we got memory back
    and this allowed an rmmod ico+modprobe ice.)
    
    When the ice module is reloaded and updated, we want the _latest_ state.
    Read the dmesg per PCI address and confirm that all addresses do not
    have safe mode.
    
    For this to work after boot, we added means to update the dmesg buffer
    if you restart zabbix_onboot (right now at most once every 10 minutes).
    
    So, if you have upgraded your linux-firmware with the fixes from
    https://bugs.launchpad.net/ubuntu/+source/linux-firmware/+bug/2138356
    you can reload the module (as long as it's unused, obviously). Follow up
    with a zabbix_onboot, and get updated (happy) values.

--- scripts/onboot.d/kernel.intel.sh
+++ scripts/onboot.d/kernel.intel.sh
@@ -14,22 +14,44 @@ if ! grep -q '^ice:' "$DMESG"; then
     exit 0
 fi
 
-if grep -q '^ice[: ].*The DDP package was successfully loaded' "$DMESG"; then
-    intel_ice_mode=ddp  # good
-else
-    intel_ice_mode=no_ddp  # unknown
-fi
-
-# Find out whether DDP loading failed. All module logs pertaining to safe mode
-# have "Entering Safe Mode".
-if grep -q '^ice[: ].*Entering Safe Mode' "$DMESG"; then
-    # This can be in conjunction "The DDP package was successfully loaded" but
-    # also without.
-    intel_ice_mode=safe_mode  # not good
-fi
+# Does this happen? Trigger if so.
+intel_ice_mode=no_ddp
+success=0
 if grep -qE '^ice[: ].*(ice_init_hw failed|[Ff]irmware recovery mode)' \
         "$DMESG"; then
-    intel_ice_mode=hw_fail    # even worse
+    intel_ice_mode=hw_fail  # figure this out later
+else
+    # Find all mentioned PCI addresses.
+    pciaddrs=$(awk \
+        '/^ice /{sub(":$","",$2);r[$2]=1} END{for(k in r){print k}}' \
+        <"$DMESG")
+
+    # For each address, check the latest of the messages.
+    for pci in $pciaddrs; do
+        intel_ice_mode=$(
+            sed -e "/^ice $pci:/"'!d;s/^ice [^ ]* //' "$DMESG" |
+            while read -r line; do
+                case $line in
+                *'The DDP package was successfully loaded'*)
+                    echo ddp;;
+                *'DDP package already present on device'*)
+                    echo ddp;;
+                *'Entering Safe Mode'*)
+                    echo safe_mode;;
+                *'ice_init_hw failed'*|*[Ff]'irmware recovery mode'*)
+                    echo hw_fail; break;;  # break fast
+                esac
+            done | tail -n1
+        )
+        test -n "$intel_ice_mode" || intel_ice_mode=no_ddp
+
+        # Shortcut the found result.
+        case $intel_ice_mode in
+            ddp) success=$((success+1));;
+            no_ddp) test "$success" -eq 0 || intel_ice_mode=ddp;;
+            hw_fail|safe_mode) break;;
+        esac
+    done
 fi
 
 # We want DDP when we have ICE (I think). So store the state and have zabbix
